a5f24f4b9b1d49f20eaad3b4253a2a29b335cd35
moroni.soares <moroni.soares@aspecti.com.br>
2019-10-22 17:49:05 -0300

    Token sendo gerado e autenticado com sucesso!!!!!!

--- Controllers/AcessoApiController.cs
+++ Controllers/AcessoApiController.cs
@@ -1,7 +1,15 @@
-﻿using AgilizaRH.Models;
+﻿using AgilizaRH.Context;
+using AgilizaRH.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using WebApi.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using WebApi.Helpers;
 
 namespace AgilizaRH.Controllers
 {
@@ -9,22 +17,39 @@ namespace AgilizaRH.Controllers
     [ApiController]
     public class AcessoApiController : ControllerBase
     {
-        private readonly IUserService _userService;
-        public AcessoApiController(IUserService userService)
+        private readonly AgilizaRHContext _context;
+        private readonly AppSettings _appSettings;
+
+        public AcessoApiController(AgilizaRHContext context, IOptions<AppSettings> appSettings)
         {
-            _userService = userService;
+            _context = context;
+            _appSettings = appSettings.Value;
         }
 
         [AllowAnonymous]
         [HttpPost("authenticate")]
-        public IActionResult Authenticate([FromBody]Usuarios model)
+        public IActionResult Authenticate([FromBody]Usuarios usuarios)
         {
-            var user = _userService.Authenticate(model.Email, model.Senha);
+            var user = _context.Usuarios.SingleOrDefault(x => x.Email == usuarios.Email && x.Senha == usuarios.Senha);
 
+            // return null if user not found
             if (user == null)
-                return BadRequest(new { message = "E-mail ou senha incorreta." });
+                return BadRequest("Credenciais inválidas...");
 
-            return Ok(user);
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_appSettings.Secret);
+            var token = new JwtSecurityToken
+                (
+                    claims: new[] { new Claim(ClaimTypes.Name, user.Id.ToString()) },
+                    expires: DateTime.Now.AddYears(10),
+                    signingCredentials: new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+                );
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+
         }
-    }
+}
 }
--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using WebApi.Helpers;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
-using WebApi.Services;
 
 namespace AgilizaRH
 {
@@ -27,9 +26,9 @@ namespace AgilizaRH
         {
             services.AddDbContext<AgilizaRHContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("AgilizaRHConnection")));
 
+            services.AddCors();
             services.AddControllers();
 
-            services.AddCors();
 
             // configure strongly typed settings objects
             var appSettingsSection = Configuration.GetSection("AppSettings");
@@ -37,12 +36,8 @@ namespace AgilizaRH
 
             // configure jwt authentication
             var appSettings = appSettingsSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
+            var key = Encoding.UTF8.GetBytes(appSettings.Secret);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(x =>
             {
                 x.RequireHttpsMetadata = false;
@@ -57,7 +52,7 @@ namespace AgilizaRH
             });
 
             // configure DI for application services
-            services.AddScoped<IUserService, UserService>();
+            //services.AddScoped(typeof(UserService), typeof(IUserService));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +67,13 @@ namespace AgilizaRH
 
             app.UseRouting();
 
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
